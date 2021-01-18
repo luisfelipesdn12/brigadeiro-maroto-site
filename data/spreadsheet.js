ce4ccbd9c4eb415f97fa1402603187c950fabf86
unknown <luisfelipesdn12@gmail.com>
2021-01-18 17:06:56 -0300

    docs: Add documentation to getQuantity function

--- data/spreadsheet.js
+++ data/spreadsheet.js
@@ -2,6 +2,15 @@ const SHEET_ID = "1JJePQcZowP0vOfdL5s6-yjHtxpKL-ghEYke4_pxL8-s";
 const PAGE = "1";
 const URL = `https://spreadsheets.google.com/feeds/cells/${SHEET_ID}/${PAGE}/public/full?alt=json`
 
+/**
+ * getQuantity get the spreadsheet data
+ * as JSON and returns a object with 
+ * flavour ID as key and the availability
+ * as value.
+ * @returns {object} quantity The object with 
+ * flavour ID as key and the availability
+ * as value.
+ */
 export async function getQuantity() {
     const quantity = {};
 
