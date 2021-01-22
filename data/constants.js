b6abc3ba3ab6aaee876946d3a15d57eb63497d47
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2021-01-22 18:23:40 -0300

    fix(data): Update phone

--- data/constants.js
+++ data/constants.js
@@ -1,5 +1,7 @@
 /**
  * The phone number which the orders will
  * be redirected to.
+ * 
+ * Old one: export const PHONE = "+55 11 98947-6716";
  */
-export const PHONE = "+55 11 98947-6716";
+export const PHONE = "+55 11 99001-2896";
